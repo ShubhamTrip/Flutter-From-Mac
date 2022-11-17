4e4fee3f26b01d6b9ba9330853978d9d77972c66
Shubham <shubhamtrip16@gmail.com>
2022-11-17 21:31:19 +0530

    new project

---
+++ applearn1/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Main());
+}
+
+class Main extends StatefulWidget {
+  const Main({super.key});
+
+  @override
+  State<Main> createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Text("Hello"),
+          ),
+        ),
+      ),
+    );
+  }
+}
