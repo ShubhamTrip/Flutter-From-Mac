2b8ccefe5d080513598fcf3d59b324714ed77b42
Shubham <shubhamtrip16@gmail.com>
2022-11-17 22:25:09 +0530

    made ques Answer

--- applearn1/lib/main.dart
+++ applearn1/lib/main.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:applearn1/question.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,14 +15,57 @@ class Main extends StatefulWidget {
 }
 
 class _MainState extends State<Main> {
+  var questions = ['What is your name?', 'Are you fine?', 'Hey Hello'];
+  var index = 0;
+  void changeQues() {
+    setState(() {
+      if (index < questions.length - 1) index++;
+    });
+  }
+
+  void lastOues() {
+    setState(() {
+      if (index > 0) index--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
           child: Center(
-            child: Text("Hello"),
-          ),
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Question(questions[index]),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 100,
+                    child: ElevatedButton(
+                        onPressed: () {
+                          lastOues();
+                        },
+                        child: Text('Previous')),
+                  ),
+                  SizedBox(width: 10),
+                  SizedBox(
+                    width: 100,
+                    child: ElevatedButton(
+                        onPressed: () {
+                          changeQues();
+                        },
+                        child: Text('Next')),
+                  )
+                ],
+              )
+            ],
+          )),
         ),
       ),
     );
---
+++ applearn1/lib/question.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  final String quesText;
+  Question(this.quesText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(quesText);
+  }
+}
