cf1e00e53828a395ee8d3b8649e256432317fd78
Shubham <shubhamtrip16@gmail.com>
2022-11-23 09:39:26 +0530

    meals app created

---
+++ mealsapp/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Main());
+}
+
+class Main extends StatefulWidget {
+  const Main({Key? key}) : super(key: key);
+
+  @override
+  State<Main> createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
